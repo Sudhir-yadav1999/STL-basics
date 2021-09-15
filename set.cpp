0e86902dcdaed3c05c4b3d7c8e453ca9ff2a6f25
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-09-15 20:13:52 +0530

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -1,9 +1,9 @@
 
-----
-Set  (Unique Values) , balanced tree , log n.
-----
+    ----
+    Set  (Unique Values) , balanced tree , log n.
+    ----
 
- int n;
+    int n;
     set<int,greater<int>> s1;//order greater to smaller
     
     for(int i=0;i<5;i++)
@@ -23,3 +23,86 @@ Set  (Unique Values) , balanced tree , log n.
     
     ----------------------------------------------------------------
     ----------------------------------------------------------------
+    
+     
+    --------------
+    Unordered_set - (unordered but unique),hashtable,o(n).
+    -------------
+    int n;
+    unordered_set<int> s1;
+    
+    for(int i=0;i<5;i++)
+    {
+        cin>>n;
+        s1.insert(n);
+    }
+
+    cout<<"The results are :::"<<endl;
+    
+    unordered_set<int>::iterator itr;
+    
+    for(itr=s1.begin();itr!=s1.end();itr++)
+    {
+        cout<<*itr<<endl;
+    }
+    -----------------------------------------------------------------
+    -----------------------------------------------------------------
+     
+     
+    --------
+    multiset -(multiple values),ordered
+    --------
+     
+    int n;
+    multiset<int> s1;
+    
+    for(int i=0;i<5;i++)
+    {
+        cin>>n;
+        s1.insert(n);
+    }
+
+    cout<<"The results are :::"<<endl;
+    
+    multiset<int>::iterator itr;
+    
+    for(itr=s1.begin();itr!=s1.end();itr++)
+    {
+        cout<<*itr<<endl;
+    }
+    ----------------------------------------------------------------- 
+    -----------------------------------------------------------------
+     
+     
+     ------------------
+     unordered_multiset -[(multivalued ),unordered,hashtable o(n)]
+     ------------------
+     
+     int n;
+    unordered_multiset<int> s1;
+    
+    for(int i=0;i<5;i++)
+    {
+        cin>>n;
+        s1.insert(n);
+    }
+
+    cout<<"The results are :::"<<endl;
+    
+    unordered_multiset<int>::iterator itr;
+    
+    for(itr=s1.begin();itr!=s1.end();itr++)
+    {
+        cout<<*itr<<endl;
+    }
+    -------------------------------------------------------------------
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
